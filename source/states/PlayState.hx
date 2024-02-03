7f3c7a66647750af3cabb186cb5da1152a881d7a
CFBGH <149869806+CFBGH@users.noreply.github.com>
2024-02-03 17:11:30 +0800

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2727,8 +2727,9 @@ class PlayState extends MusicBeatState
 		}
 	}
 
-	public function finishSong(?ignoreNoteOffset:Bool)
+	public function finishSong(?sbHaxe:Bool = false):Void
 	{
+		var ignoreNoteOffset = sbHaxe;
 		updateTime = false;
 		FlxG.sound.music.volume = 0;
 		vocals.volume = 0;
